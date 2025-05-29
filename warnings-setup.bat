1c1b32a702c15a81e333125f1de3518ce0c828b6
Rohan Jadav <ro1.darkstar7@gmail.com>
2025-05-29 22:49:43 +0530

    warnings upgrade

--- warnings-setup.bat
+++ warnings-setup.bat
@@ -1,4 +1,4 @@
 @echo off
 
-set MSVC_WARNINGS=/W4 /WX /we4013 /we4020 /we4047 /we4024 /we4028 /we4013 /we4715 /we4700 /we4716 /we4013
+set MSVC_WARNINGS=/Wall /WX
 set CLANG_WARNINGS=-Wall -Wextra -Wshadow -Wconversion -Wsign-conversion -Wdouble-promotion -Wfloat-equal -Wundef -Wswitch-enum -Wstrict-prototypes -Werror
