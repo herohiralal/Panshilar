618a11e55665f511a488d621bd0bbedc56c01ef5
Rohan Jadav <ro1.darkstar7@gmail.com>
2025-09-13 11:07:10 +0530

    minor updates to pnslr build script

--- build.py
+++ build.py
@@ -11,71 +11,53 @@ CMD_ARG_REGENERATE_BINDINGS = '-rebind'             in sys.argv # Regenerate the
 
 # region File paths ===========================================================================================================
 
-PNSLR_FOLDER_STRUCTURE = buildutils.getFolderStructure(os.path.dirname(os.path.abspath(__file__)))
+FOLDER_STRUCTURE = buildutils.getFolderStructure(os.path.dirname(os.path.abspath(__file__)))
 
-PNSLR_INTRINSICS_ROOT_DIR         = PNSLR_FOLDER_STRUCTURE.depDir + 'PNSLR_Intrinsics/'
-PNSLR_TEST_RUNNER_ROOT_DIR        = PNSLR_FOLDER_STRUCTURE.root   + 'Tools/TestRunner/'
-PNSLR_BINDINGS_GENERATOR_ROOT_DIR = PNSLR_FOLDER_STRUCTURE.root   + 'Tools/BindGen/'
+INTRINSICS_ROOT_DIR         = FOLDER_STRUCTURE.depDir + 'PNSLR_Intrinsics/'
+TEST_RUNNER_ROOT_DIR        = FOLDER_STRUCTURE.root   + 'Tools/TestRunner/'
+BINDINGS_GENERATOR_ROOT_DIR = FOLDER_STRUCTURE.root   + 'Tools/BindGen/'
 
-PNSLR_INTRINSICS_MAIN_FILE         = PNSLR_INTRINSICS_ROOT_DIR         + 'Intrinsics.c'
-PNSLR_MAIN_FILE                    = PNSLR_FOLDER_STRUCTURE.srcDir     + 'zzzz_Unity.c'
-PNSLR_TEST_RUNNER_MAIN_FILE        = PNSLR_TEST_RUNNER_ROOT_DIR        + 'zzzz_TestRunner.c'
-PNSLR_BINDINGS_GENERATOR_MAIN_FILE = PNSLR_BINDINGS_GENERATOR_ROOT_DIR + 'BindingsGenerator.c'
+INTRINSICS_MAIN_FILE         = INTRINSICS_ROOT_DIR         + 'Intrinsics.c'
+MAIN_FILE                    = FOLDER_STRUCTURE.srcDir     + 'zzzz_Unity.c'
+TEST_RUNNER_MAIN_FILE        = TEST_RUNNER_ROOT_DIR        + 'zzzz_TestRunner.c'
+BINDINGS_GENERATOR_MAIN_FILE = BINDINGS_GENERATOR_ROOT_DIR + 'BindingsGenerator.c'
 
 def getIntrinsicsObjectPath(plt: buildutils.Platform) -> str:
-    return PNSLR_INTRINSICS_ROOT_DIR + 'Prebuilt/' + buildutils.getObjectOutputFileName('intrinsics', plt)
+    return INTRINSICS_ROOT_DIR + 'Prebuilt/' + buildutils.getObjectOutputFileName('intrinsics', plt)
 
 def getLibraryObjectPath(plt: buildutils.Platform) -> str:
-    return PNSLR_FOLDER_STRUCTURE.tmpDir + buildutils.getObjectOutputFileName('unity', plt)
+    return FOLDER_STRUCTURE.tmpDir + buildutils.getObjectOutputFileName('unity', plt)
 
 def getLibraryPath(plt: buildutils.Platform) -> str:
-    return PNSLR_FOLDER_STRUCTURE.libDir + buildutils.getStaticLibOutputFileName('panshilar', plt)
+    return FOLDER_STRUCTURE.libDir + buildutils.getStaticLibOutputFileName('panshilar', plt)
 
 def getTestRunnerObjectPath(plt: buildutils.Platform) -> str:
-    return PNSLR_FOLDER_STRUCTURE.tmpDir +  buildutils.getObjectOutputFileName('testrunner', plt)
+    return FOLDER_STRUCTURE.tmpDir +  buildutils.getObjectOutputFileName('testrunner', plt)
 
 def getTestRunnerExecutablePath(plt: buildutils.Platform) -> str:
-    return PNSLR_FOLDER_STRUCTURE.binDir + buildutils.getExecOutputFileName('TestRunner', plt)
+    return FOLDER_STRUCTURE.binDir + buildutils.getExecOutputFileName('TestRunner', plt)
 
 def getBindingsGeneratorObjectPath(plt: buildutils.Platform) -> str:
-    return PNSLR_FOLDER_STRUCTURE.tmpDir +  buildutils.getObjectOutputFileName('bindgen', plt)
+    return FOLDER_STRUCTURE.tmpDir +  buildutils.getObjectOutputFileName('bindgen', plt)
 
 def getBindingsGeneratorExecutablePath(plt: buildutils.Platform) -> str:
-    return PNSLR_FOLDER_STRUCTURE.binDir + buildutils.getExecOutputFileName('BindingsGenerator', plt)
+    return FOLDER_STRUCTURE.binDir + buildutils.getExecOutputFileName('BindingsGenerator', plt)
 
 def getIntrinsicsCompileCommand(plt: buildutils.Platform) -> list[str]:
-    return buildutils.getCompilationCommand(
-        plt,
-        False,
-        PNSLR_INTRINSICS_MAIN_FILE,
-        getIntrinsicsObjectPath(plt),
-    )
+    return buildutils.getCompilationCommand(plt, False, INTRINSICS_MAIN_FILE, getIntrinsicsObjectPath(plt))
 
 def getLibraryCompileCommand(plt: buildutils.Platform) -> list[str]:
-    return buildutils.getCompilationCommand(
-        plt,
-        False,
-        PNSLR_MAIN_FILE,
-        getLibraryObjectPath(plt),
-    )
+    return buildutils.getCompilationCommand(plt, False, MAIN_FILE, getLibraryObjectPath(plt))
 
 def getLibraryLinkCommand(plt: buildutils.Platform) -> list[str]:
-    return buildutils.getStaticLibLinkCommand(
-        plt,
-        [getIntrinsicsObjectPath(plt), getLibraryObjectPath(plt)],
-        getLibraryPath(plt),
-    )
+    return buildutils.getStaticLibLinkCommand(plt, [getIntrinsicsObjectPath(plt), getLibraryObjectPath(plt)], getLibraryPath(plt))
 
 def getTestRunnerBuildCommand(plt: buildutils.Platform) -> list[str]:
     return buildutils.getExecBuildCommand(
         plt,
         True,
-        [
-            PNSLR_TEST_RUNNER_MAIN_FILE,
-            PNSLR_MAIN_FILE,
-            PNSLR_INTRINSICS_MAIN_FILE
-        ],
-        [PNSLR_FOLDER_STRUCTURE.srcDir, PNSLR_FOLDER_STRUCTURE.bndDir],
+        [TEST_RUNNER_MAIN_FILE, MAIN_FILE, INTRINSICS_MAIN_FILE],
+        [FOLDER_STRUCTURE.srcDir, FOLDER_STRUCTURE.bndDir],
         ['iphlpapi.lib', 'Ws2_32.lib'] if plt.tgt == 'windows' else ['pthread'] if plt.tgt == 'linux' else [],
         getTestRunnerExecutablePath(plt),
     )
@@ -84,12 +66,8 @@ def getBindingsGeneratorBuildCommand(plt: buildutils.Platform) -> list[str]:
     return buildutils.getExecBuildCommand(
         plt,
         True,
-        [
-            PNSLR_BINDINGS_GENERATOR_MAIN_FILE,
-            PNSLR_MAIN_FILE,
-            PNSLR_INTRINSICS_MAIN_FILE
-        ],
-        [PNSLR_FOLDER_STRUCTURE.srcDir, PNSLR_FOLDER_STRUCTURE.bndDir],
+        [BINDINGS_GENERATOR_MAIN_FILE, MAIN_FILE, INTRINSICS_MAIN_FILE],
+        [FOLDER_STRUCTURE.srcDir, FOLDER_STRUCTURE.bndDir],
         ['iphlpapi.lib', 'Ws2_32.lib'] if plt.tgt == 'windows' else [],
         getBindingsGeneratorExecutablePath(plt),
     )
@@ -99,23 +77,17 @@ def getBindingsGeneratorBuildCommand(plt: buildutils.Platform) -> list[str]:
 # region Main Logic ===========================================================================================================
 
 def buildIntrinsics(plt: buildutils.Platform) -> bool:
-    cmd = getIntrinsicsCompileCommand(plt)
-    return buildutils.runCommand(cmd, f'{plt.tgt}-{plt.arch} Intrinsics Compile')
+    return buildutils.runCommand(getIntrinsicsCompileCommand(plt), f'{plt.prettyTgt}-{plt.prettyArch} Intrinsics Compile')
 
 def buildLibrary(plt: buildutils.Platform) -> bool:
-    cmd = getLibraryCompileCommand(plt)
-    if not buildutils.runCommand(cmd, f'{plt.tgt}-{plt.arch} Library Compile'):
-        return False
-    cmd = getLibraryLinkCommand(plt)
-    return buildutils.runCommand(cmd, f'{plt.tgt}-{plt.arch} Library Link')
+    return buildutils.runCommand(getLibraryCompileCommand(plt), f'{plt.prettyTgt}-{plt.prettyArch} Library Compile') and \
+           buildutils.runCommand(getLibraryLinkCommand(plt),    f'{plt.prettyTgt}-{plt.prettyArch} Library Link')
 
 def buildTestRunner(plt: buildutils.Platform) -> bool:
-    cmd = getTestRunnerBuildCommand(plt)
-    return buildutils.runCommand(cmd, f'{plt.tgt}-{plt.arch} Test Runner Build')
+    return buildutils.runCommand(getTestRunnerBuildCommand(plt), f'{plt.prettyTgt}-{plt.prettyArch} Test Runner Build')
 
 def buildBindingsGenerator(plt: buildutils.Platform) -> bool:
-    cmd = getBindingsGeneratorBuildCommand(plt)
-    return buildutils.runCommand(cmd, f'{plt.tgt}-{plt.arch} Bindings Generator Build')
+    return buildutils.runCommand(getBindingsGeneratorBuildCommand(plt), f'{plt.prettyTgt}-{plt.prettyArch} Bindings Generator Build')
 
 # endregion
 
@@ -123,9 +95,9 @@ if __name__ == '__main__':
     buildutils.setupVsCodeLspStuff()
 
     if CMD_ARG_RUN_TESTS:
-        tests = [f.rstrip('.c') for f in os.listdir(PNSLR_TEST_RUNNER_ROOT_DIR) if f.endswith('.c') and not f.startswith('zzzz_')]
+        tests = [f.rstrip('.c') for f in os.listdir(TEST_RUNNER_ROOT_DIR) if f.endswith('.c') and not f.startswith('zzzz_')]
         tests = sorted(tests)
-        outputFilePath = os.path.join(PNSLR_TEST_RUNNER_ROOT_DIR, 'zzzz_GeneratedCombinedTests.c')
+        outputFilePath = os.path.join(TEST_RUNNER_ROOT_DIR, 'zzzz_GeneratedCombinedTests.c')
         with open(outputFilePath, 'w', encoding='utf-8') as outputFile:
             outputFile.write('// This file is generated by the build script. Do not edit it manually.\n\n')
 
@@ -149,16 +121,17 @@ if __name__ == '__main__':
             outputFile.write('}\n')
 
     for plt in buildutils.PLATFORMS:
+        cnt = True
         if CMD_ARG_REBUILD_INTRINSICS:
-            buildIntrinsics(plt)
+            cnt = cnt and buildIntrinsics(plt)
 
-        buildLibrary(plt)
+        cnt = cnt and buildLibrary(plt)
 
         if CMD_ARG_REGENERATE_BINDINGS and (plt.tgt == 'windows' or plt.tgt == 'osx'): # host platforms only
-            buildBindingsGenerator(plt)
+            cnt = cnt and buildBindingsGenerator(plt)
 
         if CMD_ARG_RUN_TESTS and (plt.tgt == 'windows' or plt.tgt == 'linux' or plt.tgt == 'osx'): # desktop platforms only
-            buildTestRunner(plt)
+            cnt = cnt and buildTestRunner(plt)
 
     buildutils.printSummary()
 
