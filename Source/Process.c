6c9d51fed71ebcb60a9700e37a0e9b6e1dba7e4b
Rohan Jadav <ro1.darkstar7@gmail.com>
2025-10-08 17:25:07 +0530

    added pipe functions

--- Source/Process.c
+++ Source/Process.c
@@ -74,3 +74,166 @@ PNSLR_ArraySlice(PNSLR_EnvVarKeyValuePair) PNSLR_GetEnvironmentVariables(PNSLR_A
     envVars.count = index; // in case of malformed entries
     return envVars;
 }
+
+b8 PNSLR_CreatePipe(PNSLR_PipeHandle* outR, PNSLR_PipeHandle* outW)
+{
+    if (outR) *outR = (PNSLR_PipeHandle) {0};
+    if (outW) *outW = (PNSLR_PipeHandle) {0};
+    if (!outR || !outW) return false;
+
+    #if PNSLR_WINDOWS
+
+        HANDLE readHandle, writeHandle;
+        SECURITY_ATTRIBUTES sa = { sizeof(SECURITY_ATTRIBUTES), NULL, TRUE };
+        if (!CreatePipe(&readHandle, &writeHandle, &sa, 0)) { return false; }
+
+        outR->platformHandle = (u64) (rawptr) readHandle;
+        outW->platformHandle = (u64) (rawptr) writeHandle;
+        return true;
+
+    #elif PNSLR_UNIX
+
+        int fds[2];
+        if (pipe(fds) != 0) { return false; }
+
+        outR->platformHandle = (u64) (i64) fds[0];
+        outW->platformHandle = (u64) (i64) fds[1];
+        return true;
+
+    #else
+
+        #error "Pipe creation not implemented on this platform"
+        return false;
+
+    #endif
+}
+
+b8 PNSLR_GetRemainingPipeReadSize(PNSLR_PipeHandle pipe, i64* outSize)
+{
+    if (!outSize)
+        return true;
+
+    #if PNSLR_WINDOWS
+
+        DWORD bytesAvailable = 0;
+        if (!PeekNamedPipe((HANDLE) (rawptr) pipe.platformHandle, NULL, 0, NULL, &bytesAvailable, NULL))
+            return false;
+
+        *outSize = (i64) bytesAvailable;
+        return true;
+
+    #elif PNSLR_UNIX
+
+        int bytesAvailable = 0;
+        if (ioctl((i32) (i64) pipe.platformHandle, FIONREAD, &bytesAvailable) != 0)
+            return false;
+
+        *outSize = (i64) bytesAvailable;
+        return true;
+
+    #else
+
+        #error "Getting remaining pipe read size not implemented on this platform"
+        return false;
+
+    #endif
+}
+
+b8 PNSLR_ReadFromPipe(PNSLR_PipeHandle pipe, PNSLR_ArraySlice(u8) dst, i64* readSize OPT_ARG)
+{
+    if (readSize) *readSize = 0;
+    if (!dst.data || !dst.count) { return false; }
+
+    b8 success = true;
+    i64 bytesReadTotal = 0;
+
+    #if PNSLR_WINDOWS
+
+        DWORD bytesRead = 0;
+        success = ReadFile((HANDLE) (rawptr) pipe.platformHandle, dst.data, (DWORD) dst.count, &bytesRead, NULL);
+        if (!success)
+        {
+            DWORD err = GetLastError();
+            if (err == ERROR_NO_DATA || err == ERROR_PIPE_NOT_CONNECTED)
+                success = true; // no data yet
+            else
+                success = false;
+            bytesRead = 0;
+        }
+        bytesReadTotal = (i64) bytesRead;
+
+    #elif PNSLR_UNIX
+
+        ssize_t res = read((i32) (i64) pipe.platformHandle, dst.data, (size_t) dst.count);
+        if (res < 0)
+        {
+            if (errno == EAGAIN || errno == EWOULDBLOCK)
+                res = 0; // no data available right now
+            else
+                success = false;
+        }
+
+        bytesReadTotal = (i64) res;
+
+    #endif
+
+    if (readSize) *readSize = bytesReadTotal;
+    return success;
+}
+
+b8 PNSLR_WriteToPipe(PNSLR_PipeHandle pipe, PNSLR_ArraySlice(u8) src)
+{
+    if (!src.data || !src.count) { return false; }
+
+    i64 bytesWrittenTotal = 0;
+    b8 success = true;
+
+    #if PNSLR_WINDOWS
+
+        DWORD bytesWritten = 0;
+        success = WriteFile((HANDLE) (rawptr) pipe.platformHandle, src.data, (DWORD) src.count, &bytesWritten, NULL);
+        if (!success)
+        {
+            DWORD err = GetLastError();
+            if (err == ERROR_NO_DATA || err == ERROR_PIPE_NOT_CONNECTED)
+                success = true; // cannot write now
+            else
+                success = false;
+            bytesWritten = 0;
+        }
+        bytesWrittenTotal = (i64) bytesWritten;
+
+    #elif PNSLR_UNIX
+
+        ssize_t res = write((i32) (i64) pipe.platformHandle, src.data, (size_t) src.count);
+        if (res < 0)
+        {
+            if (errno == EAGAIN || errno == EWOULDBLOCK)
+                res = 0; // cannot write now
+            else
+                success = false;
+        }
+
+        bytesWrittenTotal = (i64) res;
+
+    #endif
+
+    return success;
+}
+
+
+b8 PNSLR_ClosePipe(PNSLR_PipeHandle pipe)
+{
+    b8 success = true;
+    #if PNSLR_WINDOWS
+
+        success = CloseHandle((HANDLE) (rawptr) pipe.platformHandle);
+
+    #elif PNSLR_UNIX
+
+        success = (close((i32) (i64) pipe.platformHandle) == 0);
+
+    #endif
+
+    return success;
+}
--- Source/Process.h
+++ Source/Process.h
@@ -28,5 +28,51 @@ PNSLR_DECLARE_ARRAY_SLICE(PNSLR_EnvVarKeyValuePair);
  */
 PNSLR_ArraySlice(PNSLR_EnvVarKeyValuePair) PNSLR_GetEnvironmentVariables(PNSLR_Allocator allocator);
 
+/**
+ * Opaque handle to a pipe.
+ * On Windows, this is a HANDLE.
+ * On Unix-like systems, this is a file descriptor (int).
+ * This is used for inter-process communication (IPC).
+ */
+typedef struct PNSLR_PipeHandle
+{
+    u64 platformHandle;
+} PNSLR_PipeHandle;
+
+/**
+ * Creates a pipe and returns the read and write ends.
+ * The read end is used for reading data from the pipe.
+ * The write end is used for writing data to the pipe.
+ */
+b8 PNSLR_CreatePipe(PNSLR_PipeHandle* outR, PNSLR_PipeHandle* outW);
+
+/**
+ * Gets the number of bytes available to read from the read end of the pipe.
+ * The size is stored in `outSize`.
+ *
+ * Note that this function does not block, and the size may change after
+ * this function returns.
+ */
+b8 PNSLR_GetRemainingPipeReadSize(PNSLR_PipeHandle pipe, i64* outSize);
+
+/**
+ * Reads data from the read end of the pipe into the provided buffer.
+ * The number of bytes read is stored in `readSize` if it's not null.
+ * Returns true on success, false on failure.
+ */
+b8 PNSLR_ReadFromPipe(PNSLR_PipeHandle pipe, PNSLR_ArraySlice(u8) dst, i64* readSize OPT_ARG);
+
+/**
+ * Writes data to the write end of the pipe from the provided buffer.
+ * Returns true on success, false on failure.
+ */
+b8 PNSLR_WriteToPipe(PNSLR_PipeHandle pipe, PNSLR_ArraySlice(u8) src);
+
+/**
+ * Closes the pipe handle, releasing any associated resources.
+ * After calling this function, the pipe handle should not be used again.
+ */
+b8 PNSLR_ClosePipe(PNSLR_PipeHandle pipe);
+
 EXTERN_C_END
 #endif // PNSLR_PROCESS_H ==========================================================
